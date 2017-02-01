ab4dc4ae26d7f16f1ccdd2d9f8aaa907d12f5302
PeterTheOne <petertheone@gmail.com>
2017-02-01 01:32:01 +0100

    add Kontostände zu anlageverzeichnis

--- templates/anlagenv.php
+++ templates/anlagenv.php
@@ -1,79 +1,124 @@
 <h2><?php echo $title ?></h2>
-<table class="table">
-    <thead class="thead-inverse">
-        <tr>
-            <th>Anlage</th>
-            <th class="text-right text-nowrap">Anzahl</th>
-            <th class="text-right text-nowrap">Geschätzer Wert</th>
-        </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>Buttonmaschine</td>
-        <td class="text-right">1</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>A3 AIO Drucker</td>
-        <td class="text-right">1</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>Laptopdrucker</td>
-        <td class="text-right">1</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>Laptop</td>
-        <td class="text-right">1</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>iPad</td>
-        <td class="text-right">1</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>Dreiecksständer (A0, 3 teilig)</td>
-        <td class="text-right">40</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>Infotisch und Zubehör</td>
-        <td class="text-right">2</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>Zelt</td>
-        <td class="text-right">2</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>Beachflag</td>
-        <td class="text-right">3</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>Matchboxauto</td>
-        <td class="text-right">1</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>Lastenrad</td>
-        <td class="text-right">1</td>
-        <td class="text-right">-</td>
-    </tr>
-    <tr>
-        <td>Radanhänger für Plakate</td>
-        <td class="text-right">2</td>
-        <td class="text-right">-</td>
-    </tr>
-    </tbody>
-    <tfoot class="thead-inverse">
-        <tr>
-            <th>geschätzter Restwert</th>
-            <th></th>
-            <th class="text-right text-nowrap">etwa 3.500,00 €</th>
-        </tr>
-    </tfoot>
-</table>
+
+<div class="row">
+    <div class="col-lg-6">
+        <h3>Anlagen (Stand 31.01.2017)</h3>
+        <table class="table">
+            <thead class="thead-inverse">
+            <tr>
+                <th>Anlage</th>
+                <th class="text-right text-nowrap">Anzahl</th>
+                <th class="text-right text-nowrap">Geschätzer Wert</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Buttonmaschine</td>
+                <td class="text-right">1</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>A3 AIO Drucker</td>
+                <td class="text-right">1</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>Laptopdrucker</td>
+                <td class="text-right">1</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>Laptop</td>
+                <td class="text-right">1</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>iPad</td>
+                <td class="text-right">1</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>Dreiecksständer (A0, 3 teilig)</td>
+                <td class="text-right">40</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>Infotisch und Zubehör</td>
+                <td class="text-right">2</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>Zelt</td>
+                <td class="text-right">2</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>Beachflag</td>
+                <td class="text-right">3</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>Matchboxauto</td>
+                <td class="text-right">1</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>Lastenrad</td>
+                <td class="text-right">1</td>
+                <td class="text-right">-</td>
+            </tr>
+            <tr>
+                <td>Radanhänger für Plakate</td>
+                <td class="text-right">2</td>
+                <td class="text-right">-</td>
+            </tr>
+            </tbody>
+            <tfoot class="thead-inverse">
+            <tr>
+                <th>geschätzter Restwert</th>
+                <th></th>
+                <th class="text-right text-nowrap">etwa 3.500,00 €</th>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+    <div class="col-lg-6">
+        <h3>Kontostände (Stand 31.01.2017)</h3>
+        <table class="table">
+            <thead class="thead-inverse">
+            <tr>
+                <th>Konto</th>
+                <th class="text-right text-nowrap">Betrag</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Arbeit d. politischen Mandatare aka. Klubkonto</td>
+                <td class="text-right">261,59 €</td>
+            </tr>
+            <tr>
+                <td>Parteienförderungskonto <br />(inkl. für BürgerInnenbudget reservierte 60.000,00 €)</td>
+                <td class="text-right">72.645,35 €</td>
+            </tr>
+            <tr>
+                <td>Landeskonto</td>
+                <td class="text-right">416,38 €</td>
+            </tr>
+            <tr>
+                <td>Kredite, Schulden, offen Rechungen</td>
+                <td class="text-right">0,00 €</td>
+            </tr>
+            <tr>
+                <td>noch erwartetet Spenden</td>
+                <td class="text-right">400,00 €</td>
+            </tr>
+            </tbody>
+            <tfoot class="thead-inverse">
+            <tr>
+                <th>geschätzter Restwert</th>
+                <th class="text-right text-nowrap">etwa 3.500,00 €</th>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+</div>
